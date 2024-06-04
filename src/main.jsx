13c741cf1cdff0136283bbfb1fb9019db8f7b488
1manuelc <manuelwn21@gmail.com>
2024-06-04 19:58:47 -0300

    feat: new routes
    New routes for recently added pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Home from './pages/Home/index.jsx';
+import Login from './pages/Login/index.jsx';
+import Signup from './pages/Signup/index.jsx';
 import { GlobalStyle } from './global.jsx';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Home />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/signup',
+		element: <Signup />,
 	},
 ]);
 
