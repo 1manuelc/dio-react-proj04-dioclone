528c9190249e594c8f54154b34fc59fae194a86b
1manuelc <manuelwn21@gmail.com>
2024-06-12 01:39:03 -0300

    feat: login with validation in mock database

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,41 @@
-import Header from '../../components/Header';
-import { Main, Title, Subtitle, Column, Row } from '../Home/styles.jsx';
-import InputText from '../../components/InputText/index.jsx';
-import Button from '../../components/Button/index.jsx';
-import { StyledForm, Actions } from './styles.jsx';
-import { LuMail, LuLock } from 'react-icons/lu';
+import { LuLock, LuMail } from 'react-icons/lu';
 import { Link } from 'react-router-dom';
+import Button from '../../components/Button/index.jsx';
+import Header from '../../components/Header';
+import InputText from '../../components/InputText/index.jsx';
+
+import { Column, Main, Row, Subtitle, Title } from '../Home/styles.jsx';
+import { Actions, StyledForm } from './styles.jsx';
+
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { yupResolver } from '@hookform/resolvers/yup';
+import schema from '../../schema.jsx';
+import { validateLogin } from '../../utils/checkInDatabase.jsx';
 
 const Login = () => {
+	const [loginError, setLoginError] = useState('');
+	const navigate = useNavigate();
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
+
+	const onSubmit = (data) => {
+		const { email, password } = data;
+		const { canLogin, errorMessage } = validateLogin(email, password);
+
+		if (!canLogin) {
+			setLoginError(errorMessage);
+			setTimeout(() => setLoginError(''), 4000);
+		} else navigate('/userhome');
+	};
+
 	return (
 		<>
 			<Header />
@@ -26,30 +55,43 @@ const Login = () => {
 					>
 						<Title>Identifique-se</Title>
 						<Subtitle>Faça seu login e make the change._</Subtitle>
-						<StyledForm>
+						<StyledForm onSubmit={handleSubmit(onSubmit)}>
 							<InputText
 								Icon={LuMail}
 								size={20}
 								color='#8647AD'
 								placeholder='Email'
 								type='email'
+								register={register}
+								name='email'
 							/>
+							{errors.email && (
+								<p style={{ color: '#ff0000' }}>{errors.email?.message}</p>
+							)}
+
 							<InputText
 								Icon={LuLock}
 								size={20}
 								color='#8647AD'
 								placeholder='Password'
 								type='password'
+								register={register}
+								name='password'
 							/>
-						</StyledForm>
-						<Link to='/userhome' style={{ width: '100%' }}>
+							{errors.password && (
+								<p style={{ color: '#ff0000' }}>{errors.password?.message}</p>
+							)}
+
+							{loginError && <p style={{ color: '#ff0000' }}>{loginError}</p>}
+
 							<Button
 								text='Entrar'
 								variant='highlight'
 								itFillWidth
 								type='submit'
 							/>
-						</Link>
+						</StyledForm>
+						
 						<Actions>
 							<Link to='/' className='yellow'>
 								Esqueci minha senha
